b2589200e4e2c42691757750a4a121290bf9e760
Milena Cacau de Carvalho <milenacacaudecarvalho@gmail.com>
2021-03-31 17:28:37 -0300

    alterações footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,120 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import styled from 'styled-components' 
+import Logo from '.././imagens/logo/logo-branco.png'
+import Face from '.././imagens/icones/facebook.png'
+import Insta from '.././imagens/icones/insta.png'
+import Twitter from '.././imagens/icones/twitter.png'
+import Whats  from '.././imagens/icones/whats.png'
+
+
+const CorFooter = styled.div`
+    background-color: #4A2F87;     
+    height: 250px;     
+    width: 100%; 
+    display: flex;
+    justify-content: center;
+    overflow: hidden;
+`      
+
+const ContainerFooter = styled.div`
+    width: 45vw;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const ImagensFooter = styled.div`
+    width: 200px;
+`
+
+const DivIcones = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 15px;
+`
+
+const Icones = styled.img`
+`
+
+const LogoFooter = styled.img`
+    width: 200px;
+`
+
+const ContainerPaginas = styled.div`
+    display: grid;
+    grid-column-gap: 15px;
+    grid-row-gap: 10px;
+    grid-template-columns: auto auto auto;
+    width: 400px;
+    height: 150px;
+`
+
+const TituloPaginas = styled.button`
+    font-size: 20px;
+    text-align: left;
+    border: none;
+    background-color: transparent;
+    outline: none;
+    color: white;
+    opacity: 1;
+    :hover {
+        opacity: .8;
+    }
+`
+const CorCreditos = styled.div`
+    background-color: #372365;
+    height: 45px;     
+    width: 100%; 
+`
+const ContainerCreditos = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    height: 45px;
+`
+
+const TextoCreditos = styled.label`
+    color: white;  
+`
 
 class Footer extends Component {
     render (){
         return(
-            <div>Teste Footer</div>
+            <div>
+                <CorFooter>
+                    <ContainerFooter>
+                        <ImagensFooter>
+                            <LogoFooter src={Logo}/>
+
+                            <DivIcones>
+                                <Icones src={Face}/>
+                                <Icones src={Insta}/>
+                                <Icones src={Twitter}/>
+                                <Icones src={Whats}/>
+                            </DivIcones>
+                        </ImagensFooter>
+
+                        <ContainerPaginas> 
+                            <TituloPaginas>Home</TituloPaginas>
+                            <TituloPaginas>Loja 1</TituloPaginas>
+                            <TituloPaginas>Loja 3</TituloPaginas>
+                            <TituloPaginas>Área do Lojista</TituloPaginas>
+                            <TituloPaginas>Loja 2</TituloPaginas>
+                            <TituloPaginas>Loja 4</TituloPaginas>
+                            <TituloPaginas>Área do Cliente</TituloPaginas>
+                        </ContainerPaginas>
+                    </ContainerFooter>
+                </CorFooter>
+                <CorCreditos>
+                    <ContainerCreditos>
+                        <TextoCreditos> 
+                            <b>Desenvolvido por </b>Bea, Cacau, Jatobá, Madu e Musse
+                        </TextoCreditos>
+                    </ContainerCreditos>
+                </CorCreditos>
+            </div>
         )
     }
 }
--- src/pages/home/produtos.js
+++ src/pages/home/produtos.js
@@ -6,12 +6,58 @@ const ContainterProdutos = styled.div`
     min-height: 500px;
     width: 80vw;
     background-color: red;
+    margin: auto;
 `
+
+const ContainerTitulo = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+const Titulo =  styled.label`
+    font-weight: 500;
+    font-size: 35px;
+    margin-left: 20px;
+    margin-right: 20px;
+`
+
+// const ContainerTitulo = styled.div`
+//   margin-top: 10vh;
+//   margin-bottom: 10vh;
+//   justify-content: center;
+//   font-size: 30px;
+//   font-weight: 500;
+//   color: #ad2985;
+//   display: flex;
+//   position: relative;
+//     z-index: 1;
+//   &:before {
+//     border-top: 2px solid #ad2985;
+//     opacity: 0.2;
+//         content:"";
+//         margin: 0 auto;
+//         position: absolute;
+//         top: 50%; left: 0; right: 0; bottom: 0;
+//         width: 100%;
+//         z-index: -1;
+//   }
+// `
+
+// const DivTitulo = styled.div`
+//   padding: 20px;
+// `
+
 
 class ListaProdutos extends Component {
     render (){
         return(
-            <ContainterProdutos>teste</ContainterProdutos>
+            <ContainterProdutos>
+                <ContainerTitulo>
+                    <Titulo>Produtos recém publicados</Titulo>
+                </ContainerTitulo>
+                {/* <DivTitulo>Seu Carrinho</DivTitulo> */}
+
+            </ContainterProdutos>
         )
     }
 }
