c004b5fd27867b59264c6d60b5445c2487b326d5
Thiago Jatobá <tmjatob@gmail.com>
2021-03-31 17:02:50 -0300

    [feat]:Renderização do slider banner e barra de pesquisa ainda s/ funcionalidades

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,13 +3,16 @@ import {Link} from 'react-router-dom'
 import styled from 'styled-components'
 import SliderHome from './slider'
 import ListaProdutos from './produtos'
+import {Header, Footer} from '../../components'
 
 class Home extends Component {
     render (){
         return(
             <div>
+                <Header/>
                 <SliderHome/>
                 <ListaProdutos/>
+                <Footer/>
             </div>
         )
     }
