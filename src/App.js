ae5be59f61650f893b3470eafd7cae3fb07569f6
MaduBitencourt <bitencourtmadu@gmail.com>
2021-03-31 21:25:58 +0200

    Página Login

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import {Home, CadastroCliente, Carrinho, LoginCliente, NovoProduto} from './pages'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { Home, CadastroCliente, Carrinho, LoginCliente, NovoProduto } from './pages'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 
 
 
 function App() {
 	return (
-        <Router>
+		<Router>
 			<Switch>
-				<Route path='/' exact component={Home}/>
-				<Route path='/cadastro-cliente' component={CadastroCliente}/>
-				<Route path='/carrinho' component={Carrinho}/>
-				<Route path='/login-cliente' component={LoginCliente}/>
-				<Route path='/novo-produto' component={NovoProduto}/>
+				<Route path='/' exact component={Home} />
+				<Route path='/cadastro-cliente' component={CadastroCliente} />
+				<Route path='/carrinho' component={Carrinho} />
+				<Route path='/login-cliente' component={LoginCliente} />
+				<Route path='/novo-produto' component={NovoProduto} />
 			</Switch>
 		</Router>
 	)
