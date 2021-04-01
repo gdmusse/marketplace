25735ac68e86e8e0096d7d0dace814703573f477
Milena Cacau de Carvalho <milenacacaudecarvalho@gmail.com>
2021-03-31 21:12:28 -0300

    layout lista de produtos feito

--- src/pages/home/produtos.js
+++ src/pages/home/produtos.js
@@ -10,6 +10,7 @@ const ContainterProdutos = styled.div`
 
 const ContainerTitulo = styled.div`
     margin-top: 60px;
+    margin-bottom: 60px;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -19,6 +20,8 @@ const Linha = styled.hr`
     flex: auto;
     width: auto;
     border: 1px solid #AD2985;
+    background-color: #AD2985;
+    border-radius: 100px;
     opacity: 0.2;
 `
 
@@ -30,32 +33,94 @@ const Titulo =  styled.div`
     color: #AD2985;
 `
 
-// const ContainerTitulo = styled.div`
-//   margin-top: 10vh;
-//   margin-bottom: 10vh;
-//   justify-content: center;
-//   font-size: 30px;
-//   font-weight: 500;
-//   color: #ad2985;
-//   display: flex;
-//   position: relative;
-//     z-index: 1;
-//   &:before {
-//     border-top: 2px solid #ad2985;
-//     opacity: 0.2;
-//         content:"";
-//         margin: 0 auto;
-//         position: absolute;
-//         top: 50%; left: 0; right: 0; bottom: 0;
-//         width: 100%;
-//         z-index: -1;
-//   }
-// `
+const ListaDeProdutos = styled.div`
+    display: grid;
+    justify-content: space-between;
+    grid-row-gap: 75px;
+    margin-bottom: 75px;
+    grid-template-columns: 22% 22% 22% 22%;
+`
 
-// const DivTitulo = styled.div`
-//   padding: 20px;
-// `
+const CardProduto = styled.div`
+    width: 100%;
+    height: 520px;
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    :hover {
+        border-bottom: 5px solid #AD2985;
+    }
+`
 
+const DivImagem = styled.div`
+    width: 100%;
+    height: 250px;
+    position: relative;
+    float: left;
+`
+
+const ImagemProduto = styled.img`
+    width: 100%;
+    height: 250px;
+    object-fit: cover;
+`
+
+const NomeLoja = styled.div`
+    background-color: #AD2985;
+    font-size: 18px;
+    color: white;
+    border-radius: 0px 100px 0px 0px;
+    padding: 5px 0px 5px 15px;
+    width: 75%;
+    position: absolute;
+    bottom: 0;
+`
+
+const InfosProduto = styled.div`
+    width: 90%;
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    margin-top: 20px;
+    margin-bottom: 20px;
+`
+
+const NomeProduto = styled.label`
+    color: #202020;
+    font-weight: 600;
+    font-size: 25px;
+    margin-bottom: 10px;
+`
+
+const DescricaoProduto = styled.label`
+    color: #202020;
+    text-align: center;
+    font-size: 18px;
+`
+
+const PrecoProduto = styled.label`
+    color: #E44E6D;
+    font-weight: 700;
+    font-size: 25px;
+    margin: 15px;
+`
+
+const BotaoAdiciconar = styled.button`
+    border: none;
+    font-weight: 500;
+    font-size: 16px;
+    background: #FFE3EA;
+    box-shadow: 0px 3px 6px #00000012;
+    padding: 10px 25px 10px 25px;
+    border-radius: 10px;
+    :hover { 
+        background-color: #E44E6D;
+        transform: scale(1.1);
+        color: white;
+        cursor: pointer;
+    }
+
+`
 
 class ListaProdutos extends Component {
     render (){
@@ -66,8 +131,113 @@ class ListaProdutos extends Component {
                     <Titulo>Produtos Recentes</Titulo>
                     <Linha/>
                 </ContainerTitulo>
-                {/* <DivTitulo>Seu Carrinho</DivTitulo> */}
+                
+                <ListaDeProdutos>
+                    <CardProduto>
+                        <DivImagem>
+                            <ImagemProduto src={`https://picsum.photos/400/?random`}/>
+                            <NomeLoja>Nome Loja Aqui</NomeLoja>
+                        </DivImagem>
+                        <InfosProduto>
+                            <NomeProduto>Lorem ipsum dolor</NomeProduto>
+                            <DescricaoProduto>Lorem ipsum dolor sit amet, consectetur adipiscing</DescricaoProduto>
+                            <PrecoProduto>R$ 1000,00</PrecoProduto>
+                            <BotaoAdiciconar>Adicionar Produto</BotaoAdiciconar>
+                        </InfosProduto>
+                    </CardProduto>
 
+                    <CardProduto>
+                        <DivImagem>
+                            <ImagemProduto src={`https://picsum.photos/400/?random`}/>
+                            <NomeLoja>Nome Loja Aqui</NomeLoja>
+                        </DivImagem>
+                        <InfosProduto>
+                            <NomeProduto>Lorem ipsum dolor</NomeProduto>
+                            <DescricaoProduto>Lorem ipsum dolor sit amet, consectetur adipiscing</DescricaoProduto>
+                            <PrecoProduto>R$ 1000,00</PrecoProduto>
+                            <BotaoAdiciconar>Adicionar Produto</BotaoAdiciconar>
+                        </InfosProduto>
+                    </CardProduto>
+
+                    <CardProduto>
+                        <DivImagem>
+                            <ImagemProduto src={`https://picsum.photos/400/?random`}/>
+                            <NomeLoja>Nome Loja Aqui</NomeLoja>
+                        </DivImagem>
+                        <InfosProduto>
+                            <NomeProduto>Lorem ipsum dolor</NomeProduto>
+                            <DescricaoProduto>Lorem ipsum dolor sit amet, consectetur adipiscing</DescricaoProduto>
+                            <PrecoProduto>R$ 1000,00</PrecoProduto>
+                            <BotaoAdiciconar>Adicionar Produto</BotaoAdiciconar>
+                        </InfosProduto>
+                    </CardProduto>
+
+                    <CardProduto>
+                        <DivImagem>
+                            <ImagemProduto src={`https://picsum.photos/400/?random`}/>
+                            <NomeLoja>Nome Loja Aqui</NomeLoja>
+                        </DivImagem>
+                        <InfosProduto>
+                            <NomeProduto>Lorem ipsum dolor</NomeProduto>
+                            <DescricaoProduto>Lorem ipsum dolor sit amet, consectetur adipiscing</DescricaoProduto>
+                            <PrecoProduto>R$ 1000,00</PrecoProduto>
+                            <BotaoAdiciconar>Adicionar Produto</BotaoAdiciconar>
+                        </InfosProduto>
+                    </CardProduto>
+
+                    <CardProduto>
+                        <DivImagem>
+                            <ImagemProduto src={`https://picsum.photos/400/?random`}/>
+                            <NomeLoja>Nome Loja Aqui</NomeLoja>
+                        </DivImagem>
+                        <InfosProduto>
+                            <NomeProduto>Lorem ipsum dolor</NomeProduto>
+                            <DescricaoProduto>Lorem ipsum dolor sit amet, consectetur adipiscing</DescricaoProduto>
+                            <PrecoProduto>R$ 1000,00</PrecoProduto>
+                            <BotaoAdiciconar>Adicionar Produto</BotaoAdiciconar>
+                        </InfosProduto>
+                    </CardProduto>
+
+                    <CardProduto>
+                        <DivImagem>
+                            <ImagemProduto src={`https://picsum.photos/400/?random`}/>
+                            <NomeLoja>Nome Loja Aqui</NomeLoja>
+                        </DivImagem>
+                        <InfosProduto>
+                            <NomeProduto>Lorem ipsum dolor</NomeProduto>
+                            <DescricaoProduto>Lorem ipsum dolor sit amet, consectetur adipiscing</DescricaoProduto>
+                            <PrecoProduto>R$ 1000,00</PrecoProduto>
+                            <BotaoAdiciconar>Adicionar Produto</BotaoAdiciconar>
+                        </InfosProduto>
+                    </CardProduto>
+
+                    <CardProduto>
+                        <DivImagem>
+                            <ImagemProduto src={`https://picsum.photos/400/?random`}/>
+                            <NomeLoja>Nome Loja Aqui</NomeLoja>
+                        </DivImagem>
+                        <InfosProduto>
+                            <NomeProduto>Lorem ipsum dolor</NomeProduto>
+                            <DescricaoProduto>Lorem ipsum dolor sit amet, consectetur adipiscing</DescricaoProduto>
+                            <PrecoProduto>R$ 1000,00</PrecoProduto>
+                            <BotaoAdiciconar>Adicionar Produto</BotaoAdiciconar>
+                        </InfosProduto>
+                    </CardProduto>
+
+                    <CardProduto>
+                        <DivImagem>
+                            <ImagemProduto src={`https://picsum.photos/400/?random`}/>
+                            <NomeLoja>Nome Loja Aqui</NomeLoja>
+                        </DivImagem>
+                        <InfosProduto>
+                            <NomeProduto>Lorem ipsum dolor</NomeProduto>
+                            <DescricaoProduto>Lorem ipsum dolor sit amet, consectetur adipiscing</DescricaoProduto>
+                            <PrecoProduto>R$ 1000,00</PrecoProduto>
+                            <BotaoAdiciconar>Adicionar Produto</BotaoAdiciconar>
+                        </InfosProduto>
+                    </CardProduto>
+                </ListaDeProdutos>
+                
             </ContainterProdutos>
         )
     }
