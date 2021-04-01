ed2682bdd18d9ec4339ba6be2d3da409b51cc300
MaduBitencourt <bitencourtmadu@gmail.com>
2021-04-01 17:42:26 +0200

    correcao login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import { Header, Footer, DistanciaHeader} from "../components";
+import { Header, Footer, DistanciaHeader } from "../components";
 import { Titulo } from "../components"
-import { Header, Footer } from "../components";
 import "antd/dist/antd.css";
 import { message, Button, Space } from 'antd';
 
@@ -105,27 +104,27 @@ class LoginCliente extends Component {
 
 
   render() {
+    return (
+      <div>
+        <Header />
+        <DistanciaHeader />
+        <Titulo titulo='Login' />
 
-class LoginCliente extends Component {
-    render() {
-        return (
-            <div>
-              <Header/>
-              <DistanciaHeader/>
-              <Titulo titulo='Login'/>  
-              
-                <BigContainer>
-                    <Container>
-                        <Input placeholder='Nome'></Input>
-                        <Input placeholder='Email'></Input>
-                        <ButtonLogin>Efetuar Login</ButtonLogin>
-                        <div></div>
-                        <Cadastrar>Ainda não é cadastrado?</Cadastrar><CadastrarBold onClick={warning}>Clique Aqui!</CadastrarBold>
-                    </Container>
-                </BigContainer>
-              <Footer/>
-            </div>
-          }
+        <BigContainer>
+          <Container>
+            <Input placeholder='Nome'></Input>
+            <Input placeholder='Email'></Input>
+            <ButtonLogin>Efetuar Login</ButtonLogin>
+            <div></div>
+            <Cadastrar>Ainda não é cadastrado?</Cadastrar><CadastrarBold onClick={warning}>Clique Aqui!</CadastrarBold>
+          </Container>
+        </BigContainer>
+        <Footer />
+      </div>
+    )
+
+  }
+}
 
 
 export default LoginCliente
